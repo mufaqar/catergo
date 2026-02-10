81ba0047ed4b2f53dcc879969d43da9881347a94
Mufaqar <mufaqar@gmail.com>
2026-02-10 14:09:38 +0500

    updated

--- inc/store.php
+++ inc/store.php
@@ -68,7 +68,7 @@ function cptui_register_my_taxes_location() {
 		"show_in_nav_menus" => true,
 		"query_var" => true,
 		"rewrite" => [ 'slug' => 'location', 'with_front' => true,  'hierarchical' => true, ],
-		"show_admin_column" => false,
+		"show_admin_column" => true,
 		"show_in_rest" => true,
 		"show_tagcloud" => true,
 		"rest_base" => "location",
