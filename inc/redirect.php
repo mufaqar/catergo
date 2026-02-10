7cb24161d9c654ec90d45cc28fbd50ca1499e333
Mufaqar <mufaqar@gmail.com>
2026-02-10 16:23:21 +0500

    redriect

--- inc/redirect.php
+++ inc/redirect.php
@@ -58,5 +58,5 @@ add_action('template_redirect', function() {
 
 // 4️⃣ Call the function with your pages
 add_action('init', function() {
-    add_location_rewrite_for_pages(['all-caterers','menus','bestallning']);
+  //  add_location_rewrite_for_pages(['all-caterers','menus','bestallning']);
 });
