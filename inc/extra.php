01084147194cae821527c754819598da07a65a7a
Mufaqar <mufaqar@gmail.com>
2026-02-18 19:26:32 +0500

    exra

--- inc/extra.php
+++ inc/extra.php
@@ -1,8 +1,8 @@
 <?php
 
-    include_once('ajax_calls.php');
-  include_once('store.php');
-  include_once('request.php');
+   // include_once('ajax_calls.php');
+  //include_once('store.php');
+  //include_once('request.php');
    //include_once('redirect.php');
 
  
