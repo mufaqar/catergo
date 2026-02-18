c8a0ca4f5ef49ca0546fd63a02b3fa539c5cae96
Mufaqar <mufaqar@gmail.com>
2026-02-18 19:27:56 +0500

    store

--- inc/extra.php
+++ inc/extra.php
@@ -1,7 +1,7 @@
 <?php
 
-   // include_once('ajax_calls.php');
-  //include_once('store.php');
+   include_once('ajax_calls.php');
+   include_once('store.php');
   //include_once('request.php');
    //include_once('redirect.php');
 
