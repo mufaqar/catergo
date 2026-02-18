f4af9b817815a39a3bc77be86202837098023d23
Mufaqar <mufaqar@gmail.com>
2026-02-18 19:30:02 +0500

    commit

--- home.php
+++ home.php
@@ -1,24 +1,24 @@
 <?php
 /* Template Name: Home */
 
-// REDIRECT LOGIC - MUST BE AT THE VERY TOP, BEFORE ANY OUTPUT
-// if (is_front_page() || is_home()) {
-//     if (!isset($_GET['skip_location_redirect']) || $_GET['skip_location_redirect'] != '1') {
-//         if (!empty($_COOKIE['selected_location'])) {
-//             $location_slug = sanitize_text_field($_COOKIE['selected_location']);
-//             $term = get_term_by('slug', $location_slug, 'location');
 
-//             if ($term && !is_wp_error($term)) {
-//                 $location_url = get_term_link($term);
+if (is_front_page() || is_home()) {
+    if (!isset($_GET['skip_location_redirect']) || $_GET['skip_location_redirect'] != '1') {
+        if (!empty($_COOKIE['selected_location'])) {
+            $location_slug = sanitize_text_field($_COOKIE['selected_location']);
+            $term = get_term_by('slug', $location_slug, 'location');
 
-//                 if (!is_wp_error($location_url)) {
-//                     wp_redirect($location_url);
-//                     exit;
-//                 }
-//             }
-//         }
-//     }
-// }
+            if ($term && !is_wp_error($term)) {
+                $location_url = get_term_link($term);
+
+                if (!is_wp_error($location_url)) {
+                    wp_redirect($location_url);
+                    exit;
+                }
+            }
+        }
+    }
+}
 
 // NOW start the header and output
 get_header(); ?>
