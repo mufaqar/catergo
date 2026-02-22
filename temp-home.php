da51198c74938dd8d5db6c064822f618ba8faef1
Mufaqar <mufaqar@gmail.com>
2026-02-22 17:37:34 +0500

    Home page

--- temp-home.php
+++ temp-home.php
@@ -2,23 +2,23 @@
 /* Template Name: HomePage */
 
 
-exit("Home page");
 
-    if (!isset($_GET['skip_location_redirect']) || $_GET['skip_location_redirect'] != '1') {
-        if (!empty($_COOKIE['selected_location'])) {
-            $location_slug = sanitize_text_field($_COOKIE['selected_location']);
-            $term = get_term_by('slug', $location_slug, 'location');
 
-            if ($term && !is_wp_error($term)) {
-                $location_url = get_term_link($term);
+    // if (!isset($_GET['skip_location_redirect']) || $_GET['skip_location_redirect'] != '1') {
+    //     if (!empty($_COOKIE['selected_location'])) {
+    //         $location_slug = sanitize_text_field($_COOKIE['selected_location']);
+    //         $term = get_term_by('slug', $location_slug, 'location');
 
-                if (!is_wp_error($location_url)) {
-                    wp_redirect($location_url);
-                    exit;
-                }
-            }
-        }
-    }
+    //         if ($term && !is_wp_error($term)) {
+    //             $location_url = get_term_link($term);
+
+    //             if (!is_wp_error($location_url)) {
+    //                 wp_redirect($location_url);
+    //                 exit;
+    //             }
+    //         }
+    //     }
+    // }
 
 
 // NOW start the header and output
@@ -26,7 +26,7 @@ get_header(); ?>
 
 <?php 
 // Debug output - only shown if no redirect happened
-echo "Home Page Template Loaded Successfully";
+echo "Home Page Tempy";
 ?>
 
 <!-- Rest of your home page content here -->
