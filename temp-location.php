3103d303b5bc1488fd2a308559fb28fda124bc89
Mufaqar <mufaqar@gmail.com>
2026-02-22 19:27:45 +0500

    updated

---
+++ temp-location.php
@@ -0,0 +1,30 @@
+<?php /* Template Name: Location Archive */ get_header(); 
+
+
+
+
+$term = get_queried_object();
+
+// echo $term->term_id;
+// echo $term->name;
+ //echo $term->slug;
+//echo $term->taxonomy;
+
+print_r($term);
+
+
+
+?>
+
+
+ 
+ <?php get_template_part('partials/home', 'sliders'); ?>
+ <?php get_template_part('partials/home', 'categories'); ?>
+  <?php get_template_part('partials/home', 'banner'); ?>
+ <?php get_template_part('partials/home', 'products'); ?>
+ <?php //get_template_part('partials/home', 'mission'); ?>
+  <?php //get_template_part('partials/home', 'testimonials'); ?>
+ <?php get_template_part('partials/home', 'booking'); ?>
+
+
+<?php get_footer(); ?>
