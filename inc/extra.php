2c2d688518c46aff62f235addf904ab8991f64a9
Mufaqar <mufaqar@gmail.com>
2026-02-22 17:15:17 +0500

    push

--- inc/extra.php
+++ inc/extra.php
@@ -1,10 +1,10 @@
 <?php
 
-include_once('woo.php');
-include_once('ajax_calls.php');
-include_once('store.php');
-include_once('request.php');
-include_once('redirect.php');
+// include_once('woo.php');
+// include_once('ajax_calls.php');
+// include_once('store.php');
+// include_once('request.php');
+// include_once('redirect.php');
 
 
 
