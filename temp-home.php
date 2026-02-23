ac12f6692a446e6ac097a5031a3cf04f1ddfbd7d
Mufaqar <mufaqar@gmail.com>
2026-02-23 15:34:43 +0500

    stock

--- temp-home.php
+++ temp-home.php
@@ -5,21 +5,29 @@
 
 if (!isset($_GET['skip_location_redirect']) || $_GET['skip_location_redirect'] != '1') {
 
-    if (!empty($_COOKIE['selected_location'])) {
+    // Default location
+    $location_slug = 'stockholm';
 
-        $location_slug = sanitize_text_field($_COOKIE['selected_location']);
-        $term = get_term_by('slug', $location_slug, 'location');
+    // If cookie exists, override default
+    if (!empty($_COOKIE['selected_location'])) {
+        $cookie_slug = sanitize_text_field($_COOKIE['selected_location']);
+
+        $term = get_term_by('slug', $cookie_slug, 'location');
 
         if ($term && !is_wp_error($term)) {
-
-            $location_url = home_url('/' . $term->slug . '/');
-
-            wp_redirect($location_url);
-            exit;
+            $location_slug = $term->slug;
         }
     }
-}
 
+    // Get final term (either cookie location OR stockholm fallback)
+    $term = get_term_by('slug', $location_slug, 'location');
+
+    if ($term && !is_wp_error($term)) {
+        $location_url = home_url('/' . $term->slug . '/');
+        wp_redirect($location_url);
+        exit;
+    }
+}
 
 // NOW start the header and output
 get_header(); ?>
